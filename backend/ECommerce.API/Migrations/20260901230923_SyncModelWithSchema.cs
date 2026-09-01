346d3cc86bfe164eea20c6dbf988c49e80c38ee5
Oğuz Ordu <oguzordu.0@gmail.com>
2026-09-02 02:16:26 +0300

    Model ile migration arasındaki farkı kapatan migration ekledim
    UserSettings tablosundan modelde olmayan EmailNotifications kolonu düşürüldü.
    Bu eksik olduğu için dotnet ef database update PendingModelChangesWarning
    verip veritabanı kurulumunu engelliyordu.

---
+++ backend/ECommerce.API/Migrations/20260901230923_SyncModelWithSchema.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModelWithSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailNotifications",
+                table: "UserSettings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EmailNotifications",
+                table: "UserSettings",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
